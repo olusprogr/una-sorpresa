78d976b435ab6f9681c608fb0b1a73c1129b4c35
olusprogr <olus.main333@gmail.com>
2024-10-11 20:53:31 +0200

    awdada

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const port = 3000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://olusmain:paR0r7oIQ82eM9PI@cluster0.ztby1wg.mongodb.net/?retryWrites=true&w=majority";
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req, res) => {
   res.send({ message: 'OK! passt!' })
