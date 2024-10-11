08fd432f9601f2d1bb5f196a5c9512e7b7be2c48
olusprogr <olus.main333@gmail.com>
2024-10-11 20:46:38 +0200

    awdaad

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://olusmain:paR0r7oIQ82eM9PI@cluster0.ztby1wg.mongodb.net/?retryWrites=true&w=majority";
+
+
 app.get('/', (req, res) => {
-  res.send({ message: 'OK! working!' })
+  res.send({ message: 'OK! passt!' })
 
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(ip)
+
+  const database = client.db('iptracker');
+  const collection = database.collection('ips');
+  collection.insertOne({ ip: ip, date: new Date() });
 })
 
 app.listen(port, () => {
