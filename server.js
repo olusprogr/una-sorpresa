7b3b8f1e8e90c2e218785bd21188e6ea43eec11b
olusprogr <olus.main333@gmail.com>
2024-10-12 08:56:39 +0200

    awdwawad

--- server.js
+++ server.js
@@ -14,16 +14,21 @@ const client = new MongoClient(uri, {
 });
 
 app.get('/', (req, res) => {
-  res.send({ message: 'OK! passt!' })
+  res.send({ message: 'Error! Seite konnte nicht geladen werden! Versuche erneut!' })
 
   console.log(req);
 
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(ip)
+  const requestInfo = {
+    ip: ip,
+    userAgent: req.headers['user-agent'],
+    referer: req.headers['referer'],
+    date: new Date()
+  };
 
   const database = client.db('iptracker');
   const collection = database.collection('ips');
-  collection.insertOne({ information: req, date: new Date() });
+  collection.insertOne({date: new Date(), information: requestInfo});
 })
 
 app.listen(port, () => {
